0d71dd67939b5fe5c56047f136614dd9f395b21c
Jefferson Chen <f1jefferson@yahoo.com.tw>
2021-11-14 01:13:27 +0800

    package upgraded

---
+++ src/Components/ShutdownScreen/ContactPanel/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+import ContactWindow from "../../MainMenu/src/ContactWindow"
+
+const ContactPanel = () => {
+    return (
+        <div>
+            <ContactWindow />
+        </div>
+    );
+}
+
+export default ContactPanel;
