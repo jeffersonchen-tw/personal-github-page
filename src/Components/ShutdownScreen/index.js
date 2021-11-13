c1c8d0422bbbab8c0360631535db4c9b39e1399b
Jefferson Chen <f1jefferson@yahoo.com.tw>
2021-11-13 17:25:48 +0800

    boot screen selected item stle added

--- src/Components/ShutdownScreen/index.js
+++ src/Components/ShutdownScreen/index.js
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { FaPowerOff } from "react-icons/fa";
 import "./style.css";
 
 const ShutdownScreen = () => {
+	// for navigation
+	const history = useHistory();
+	const restart = () => {
+		history.push("/");
+	}
+	// for list item selection
 	const [selected, setSelected] = useState(0);
+	const isSelected = (index) => {
+		if (index === selected) {
+			return "true"
+		}
+		return "false"
+	}
+
 	return (
 		<div className="background">
 			<div className="main-panel">
@@ -13,17 +27,17 @@ const ShutdownScreen = () => {
 					</div>
 					<div className="list-container">
 						<ul className="list">
-							<li>resume</li>
-							<li>source code</li>
-							<li>send email</li>
+							<li is-selected={isSelected(0)}>resume</li>
+							<li is-selected={isSelected(1)}>source code</li>
+							<li is-selected={isSelected(2)}>send email</li>
 						</ul>
 					</div>
 				</div>
-				<div className="power-btn">
+				<div className="power-btn" onClick={restart}>
 					<FaPowerOff style={{
 						color: '#c0bdb8',
 						fontSize: '1.8em'
-					}} />
+					}}/>
 				</div>
 			</div>
 		</div>
