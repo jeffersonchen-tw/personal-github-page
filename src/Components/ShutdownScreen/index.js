e927360e360a00e8f4066bac757ca7bb83f05613
Jefferson Chen <f1jefferson@yahoo.com.tw>
2021-11-13 20:34:57 +0800

    initial a contact panel for shutdown screen

--- src/Components/ShutdownScreen/index.js
+++ src/Components/ShutdownScreen/index.js
@@ -1,45 +1,10 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { FaPowerOff } from "react-icons/fa";
 import "./style.css";
+import MainPanel from "./src/MainPanel";
 
 const ShutdownScreen = () => {
-	// for navigation
-	const history = useHistory();
-	const restart = () => {
-		history.push("/");
-	}
-	// for list item selection
-	const [selected, setSelected] = useState(0);
-	const isSelected = (index) => {
-		if (index === selected) {
-			return "true"
-		}
-		return "false"
-	}
-
 	return (
 		<div className="background">
-			<div className="main-panel">
-				<div className="option-box">
-					<div className="title-bar">
-						<div>Boot Option</div>
-					</div>
-					<div className="list-container">
-						<ul className="list">
-							<li is-selected={isSelected(0)}>resume</li>
-							<li is-selected={isSelected(1)}>source code</li>
-							<li is-selected={isSelected(2)}>send email</li>
-						</ul>
-					</div>
-				</div>
-				<div className="power-btn" onClick={restart}>
-					<FaPowerOff style={{
-						color: '#c0bdb8',
-						fontSize: '1.8em'
-					}}/>
-				</div>
-			</div>
+			<MainPanel />
 		</div>
 	)
 }
